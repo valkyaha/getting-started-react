26cdb5c4d475a8c7656abdea3299a11fc6fa6f9e
Miguel Ángel López <darkvalhalla@protonmail.ch>
2022-10-07 16:49:27 +0200

    RGS2.2

--- src/index.js
+++ src/index.js
@@ -3,16 +3,43 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
+const testData = [
+    {
+        "avatar_url": "https://avatars.githubusercontent.com/u/26414043?v=4",
+        "name": "Miguel Ángel López",
+        "company": "Inno-it",
+    }
+]
+
+
+const CardList = (props) => (
+    <div>
+        {props.profiles.map(profile => <Card {...profile}/>)}
+    </div>
+)
+
 class Card extends React.Component {
     render() {
+        const profile = this.props;
         return (
-            <div className="github-profile">
-                <img src="https://placehold.it/75"/>
-                <div className="info">
-                    <div className="name"> Name here ...</div>
-                    <div className="company"> Company here ...</div>
+            <div className="github-profile" style={{margin: '1rem'}}>
+                <img src={profile.avatar_url} alt=""/>
+                <div className="info" style={{display: "inline-block", marginLeft: 10}}>
+                    <div className="name" style={{fontSize: '125%'}}> {profile.name}</div>
+                    <div className="company"> {profile.company}</div>
                 </div>
             </div>
+        );
+    }
+}
+
+class Form extends React.Component{
+    render() {
+        return(
+            <form action="">
+                <input type="text" placeholder="GitHub username"/>
+                <button>Add card</button>
+            </form>
         );
     }
 }
@@ -22,15 +49,12 @@ class App extends React.Component {
         return (
             <div>
                 <div className="header">{this.props.title}</div>
-                <Card />
+                <Form/>
+                <CardList profiles={testData}/>
             </div>
         );
     }
 }
-
-// const App = ({title}) => (
-//     <div className="header">{title}</div>
-// );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -39,7 +63,4 @@ root.render(
     </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
