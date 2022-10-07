3bc56a8b25a8f895824ce1bf52c982fae5fc7334
Miguel Ángel López <darkvalhalla@protonmail.ch>
2022-10-07 16:30:54 +0200

    RGS2.1

--- src/index.js
+++ src/index.js
@@ -1,14 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+class Card extends React.Component {
+    render() {
+        return (
+            <div className="github-profile">
+                <img src="https://placehold.it/75"/>
+                <div className="info">
+                    <div className="name"> Name here ...</div>
+                    <div className="company"> Company here ...</div>
+                </div>
+            </div>
+        );
+    }
+}
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="header">{this.props.title}</div>
+                <Card />
+            </div>
+        );
+    }
+}
+
+// const App = ({title}) => (
+//     <div className="header">{title}</div>
+// );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App title={"The gitHubPage cards app"}/>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
