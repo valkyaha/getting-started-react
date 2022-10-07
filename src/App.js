e126c30493140c740a3a38831b859d9a11cab7d9
Miguel Ángel López <darkvalhalla@protonmail.ch>
2022-10-07 15:29:59 +0200

    RGS1.4

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+
+function Button(props) {
+  return (
+      <button onClick={props.incrementCounter}>
+        +1
+      </button>
+  );
+}
+
+function Display(props) {
+  return (
+      <div>{props.messageProp}</div>
+  );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    const [counter, setCounter] = useState(42);
+    const incrementCounter = () => setCounter(counter+1);
+    return (
+      <div>
+        <Button incrementCounter={incrementCounter}/>
+        <Display messageProp={counter}/>
+      </div>
   );
 }
 
