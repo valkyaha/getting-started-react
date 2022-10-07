727dda243df68f29a5ffc7ed1109352c9c123427
Miguel Ángel López <darkvalhalla@protonmail.ch>
2022-10-07 15:36:46 +0200

    RGS1.5

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import './App.css';
 import {useState} from "react";
 
 function Button(props) {
-  return (
-      <button onClick={props.incrementCounter}>
-        +1
+    const handleClick = () => props.onClickFunction(props.increment);
+    return (
+      <button onClick={handleClick}>
+          +{props.increment}
       </button>
   );
 }
@@ -16,11 +17,14 @@ function Display(props) {
 }
 
 function App() {
-    const [counter, setCounter] = useState(42);
-    const incrementCounter = () => setCounter(counter+1);
+    const [counter, setCounter] = useState(0);
+    const incrementCounter = (incrementValue) => setCounter(counter+incrementValue);
     return (
       <div>
-        <Button incrementCounter={incrementCounter}/>
+        <Button onClickFunction={incrementCounter} increment={1}/>
+        <Button onClickFunction={incrementCounter} increment={5}/>
+        <Button onClickFunction={incrementCounter} increment={10}/>
+        <Button onClickFunction={incrementCounter} increment={100}/>
         <Display messageProp={counter}/>
       </div>
   );
