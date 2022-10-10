3b12e3afe84d46120dfe3dba65fedb8c6c8d8929
miguellopez <mlopez@inno-it.es>
2022-10-10 13:11:05 +0200

    Done with React basics

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import React, {useState} from "react";
+import Game from "./component/Game";
+
+
+const StarMatch = () => {
+    const [gameId, setGame] = useState(1);
+    return <Game key={gameId} startNewGame={() => setGame(gameId + 1)}/>;
+}
+
 function App() {
     return (
-      <div>
-      </div>
-  );
+        <StarMatch/>
+    );
 }
 
 export default App;
